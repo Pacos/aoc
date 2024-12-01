85775231f03cbb99c2b932b7837dbefe643ed78c
Pascal HALTER <pascos@gmail.com>
2024-12-01 13:41:22 +0100

    aoc 2024: day1 - observability

--- 2024/day1.py
+++ 2024/day1.py
@@ -1,5 +1,9 @@
 from collections import Counter
 
+from loguru import logger
+
+from tools.observability import monitor_exec_time
+
 def parse_data(filename):
     with open(filename) as f:
         lefts = []
@@ -10,17 +14,19 @@ def parse_data(filename):
             rights.append(int(elems[1]))
     return lefts, rights
 
+@monitor_exec_time
 def compute_a(lefts, rights):
     lefts = sorted(lefts)
     rights = sorted(rights)
     return sum([abs(lefts[i] - rights[i]) for i in range(len(lefts))])
 
+@monitor_exec_time
 def compute_b(lefts, rights):
     r_occ = Counter(rights)
     return sum([item * r_occ[item] for item in lefts])
 
 if __name__ == "__main__":
     lefts, rights = parse_data("data/day1")
-    print(f"Day 1a > {compute_a(lefts, rights)}")
-    print(f"Day 1b > {compute_b(lefts, rights)}")
+    logger.info(f"Day 1a > {compute_a(lefts, rights)}")
+    logger.info(f"Day 1b > {compute_b(lefts, rights)}")
 
---
+++ 2024/tools/observability.py
@@ -0,0 +1,17 @@
+import functools
+import time
+
+from loguru import logger
+
+
+def monitor_exec_time(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter_ns()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter_ns()
+        run_time = (end_time - start_time)/1000
+        logger.info(f"Runtime for function {func.__name__}: {run_time}ms")
+        return value
+
+    return wrapper
